21bab4bd3e79756935b6637a4dd05be42277ca62
Pronothurah <onsongoonditi@gmail.com>
2023-07-14 04:03:28 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,11 @@
 #include <time.h>
 #include<stdio.h>
 
-/*
- *
- *main - Entry point
- *Description: 'Check for number if positive or negative'
- *Return: always 0
- *
- */
+ /**
+  *main - Entry point
+  *Description: 'Check for number if positive or negative'
+  *Return: always 0
+  */
 int main(void)
 {
 	int n;
