4ab17206f30e379a83e2c916a7fe9b01cd45b5ea
Pronothurah <onsongoonditi@gmail.com>
2023-07-14 11:57:29 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints all possible different combinations of three digits'
+ * Return: always 0
+ */
+int main(void)
+{
+	return (0);
+}
