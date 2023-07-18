90749ccd4894735b6da115a92e3a586f0f5cd4f5
Pronothurah <onsongoonditi@gmail.com>
2023-07-18 09:18:37 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints alphabet in lowercase
+ * print_alphabet_x10 - Prints alphabet in ten times
  *
  * Return: always 0
  */
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 
 	for (j = 1; i <= 10; i++)
 	{
-		for (j = 97; j <= 122; i++)
+		for (j = 97; j <= 122; j++)
 		{
 			_putchar(j);
 		}
