e7c97fb1434aeaebe495e6825067e4912185666a
Pronothurah <onsongoonditi@gmail.com>
2023-07-18 12:57:06 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - returns minutes of a day
+ *
+ * Return: no return
+ *
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (a <= 2 && b <= 4)
+						break;
+					_putchat(a + 48);
+					_putchat(b + 48);
+					_putchat(58);
+					_putchat(c + 48);
+					_putchat(d + 48);
+					_putchat('\n');
+				}
+			}
+		}
+	}
+}
