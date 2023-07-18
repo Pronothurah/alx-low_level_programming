a69eb16589949191f5b8800d9cbba9cc35fb1a43
Pronothurah <onsongoonditi@gmail.com>
2023-07-18 11:01:35 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints plus if greater than zero
+ * 0 if its equal to zero and 1 minus if less than zero
+ *
+ * @n: An ASCII character
+ *
+ * Return: 1 if plus 0 if equal 0 and - if negative
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar(43);
+		return (0);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
