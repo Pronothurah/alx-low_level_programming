bd819715c81bb8ed8da5a1260c9f939ee0bfacb7
Pronothurah <onsongoonditi@gmail.com>
2023-07-19 06:44:11 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,8 +10,36 @@
  */
 void print_times_table(int n)
 {
-	int n;
+	int prod, mult, num;
 
-	if (n > 15 || n < 0)
-		return (0);
+	if (n >= 15 && n <= 0)
+	{
+		for (num = n; num <= 15; num++)
+		{
+			_putchar(48);
+			for (mult = n; mult <= 15; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + 48);
+					_putchar((prod % 10) + 48);
+				}
+			}
+			_putchar('\n');		}
+	}
 }
