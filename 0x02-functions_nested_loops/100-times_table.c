169a6760eed160d8077ada3e5d1d2abc1f36658f
Pronothurah <onsongoonditi@gmail.com>
2023-07-19 06:17:51 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ *
+ * @n: starting input
+ *
+ * Return: f n is greater than 15 or less than 0 the
+ * function should not print anything
+ */
+void print_times_table(int n)
+{
+	int n;
+
+	if (n > 15 || n < 0)
+		return (0);
+}
