9af69538327470050cceb35b5b29309431a5d4b8
Pronothurah <onsongoonditi@gmail.com>
2023-07-19 14:51:29 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,7 +4,7 @@
 /**
  * numLength - returns length of string
  * @num: operand number
- * return: number of digits
+ * Return: number of digits
  */
 
 int numLength(int num)
@@ -31,7 +31,8 @@ int numLength(int num)
 int main(void)
 {
 	int count, initials;
-	unsigned long f1 = 1, f2 = 2, sum, max = 100000000, f1o = 0, f2o = 0, sumo = 0;
+	unsigned long f1 = 1, f2 = 2,
+		      sum, max = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
 	for (count = 1; count <= 98; count++)
 	{
