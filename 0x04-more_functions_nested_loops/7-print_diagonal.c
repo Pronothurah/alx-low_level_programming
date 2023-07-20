dcf2afb123c81a9930a82f8033d90cffcd4c0118
Pronothurah <onsongoonditi@gmail.com>
2023-07-20 05:25:46 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ *
+ * @n: number of times character should be printed
+ */
+void print_diagonal(int n)
+{
+	int pos, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (pos = 1; pos <= n; pos++)
+		{
+			for (space = 1; space <= pos; space++)
+				_putchar(' ');
+			_putchar('/');
+			_putchar('\n');
+		}
+	}
+}
