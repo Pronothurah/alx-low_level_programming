f807af4aba959588a2273af59e30b0b07e2becf1
Pronothurah <onsongoonditi@gmail.com>
2023-07-20 04:56:50 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the 10x numbers, from 0 to 14, followed by a new line
+ *
+ * Return: No return value (void)
+ */
+
+void more_numbers(void)
+{
+	int i, j, num;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			num = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				num = j % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
+}
