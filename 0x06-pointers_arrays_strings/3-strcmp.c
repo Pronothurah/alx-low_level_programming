83ba5c96252cbf037793ef32c70810fe8549be3e
Pronothurah <onsongoonditi@gmail.com>
2023-07-26 05:39:58 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int equal = 0;
-
-	while (*s1)
+	while (*s1 && *s2)
 	{
 		if (*s1 != *s2)
 		{
-			equal = ((int) * s1 - 48) - ((int) * s2 -48);
-			break;
+			return (*s1 - *s2);
 		}
 		s1++;
 		s2++;
 	}
 
-	return (equal);
+	return (*s1 - *s2);
 }
