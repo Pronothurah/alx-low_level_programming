6ecdac466751f3d84e4c81b8feec89e3abdd614e
Pronothurah <onsongoonditi@gmail.com>
2023-07-26 03:21:54 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @src: input string
+ * @dest: input string
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
