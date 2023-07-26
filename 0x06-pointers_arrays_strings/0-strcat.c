8e0bc245e1fc6fe0f1c429511f932186a0683924
Pronothurah <onsongoonditi@gmail.com>
2023-07-26 03:39:28 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,3 +8,15 @@
  * Return: a pointer to the resulting string dest
  */
 char *_strcat(char *dest, char *src)
+{
+	int i;
+	int dest_len = strlen(dest);
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
