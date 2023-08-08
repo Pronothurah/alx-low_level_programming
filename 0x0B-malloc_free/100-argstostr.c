83f697a47de37cc7f828887f0449e6bd8bf9075a
Pronothurah <onsongoonditi@gmail.com>
2023-08-08 14:27:21 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,7 +25,23 @@ int _strlen(char *s)
 char *argstostr(int ac, char **av)
 {
 	int i = 0, j = 0, new_count = 0, cmpt = 0;
+	char *s;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+	for (; i < ac; i++, new_count++)
+		new_count += _strlen(av[i]);
+	s = malloc(sizeof(char) * new_count + 1);
+
+	if (s == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+			s[cmpt] = av[i][j];
+		s[cmpt] = '\n';
+		cmpt++;
+	}
+	return (s);
 }
