00c5774244928f99bfe6e34feda6ed91b83c9729
Pronothurah <onsongoonditi@gmail.com>
2023-08-10 07:12:56 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,17 +20,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (s1_lgth = 0; s1[s1_lgth] != '\0'; s1_lgth++)
 		;
 	for (s2_lgth = 0; s2[s2_lgth] != '\0'; s2_lgth++)
-                ;
+		;
 
 	str = malloc(s1_lgth + n + 1);
 	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s2[i];
+		str[i] = s1[i];
 	for (j = 0; j < n; j++)
 	{
-                str[j] = s2[j];
+		str[i] = s2[j];
 		i++;
 	}
 	str[i] = '\0';
