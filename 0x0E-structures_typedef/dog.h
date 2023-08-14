e479174327545c296c9d5e7aabb4174624351d93
Pronothurah <onsongojosphat28@gmail.com>
2023-08-14 16:30:24 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,20 @@
 #ifndef DOG_H
+#define DOG_H
 
-struct dog {
-	char * name;
+/**
+ * struct dog - represent information about a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * This struct defines the characteristics of a dog
+ */
+
+struct dog
+{
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 #endif /* DOG_H */
