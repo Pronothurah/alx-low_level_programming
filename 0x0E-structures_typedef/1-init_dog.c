a50f5185f450e7f8ce6254cd9224d92af6c5c824
Pronothurah <onsongojosphat28@gmail.com>
2023-08-15 12:55:50 +0000

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -20,7 +20,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		strcpy(d->name, name);
 	}
 	d->age = age;
-	d->owner = malloc(strlen(name) + 1);
+	d->owner = malloc(strlen(owner) + 1);
 	if (d->owner)
 	{
 		strcpy(d->owner, owner);
