f6910958347bb5b2b4ac08871e15be8423be9a1f
Pronothurah <onsongojosphat28@gmail.com>
2023-08-16 20:13:36 +0000

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: first int
+ * @b: second int
+ * Return: sum of ints
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: first integers
+ * @b: second integer
+ * Return: difference of ints
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: product of ints
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: division of ints
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: remainder of ints
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
