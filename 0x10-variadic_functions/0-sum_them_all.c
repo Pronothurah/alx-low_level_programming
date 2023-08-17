d92ec636ebbbdf6cdab1b311bfe0612cbdec9b72
Pronothurah <onsongojosphat28@gmail.com>
2023-08-17 19:53:42 +0000

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all its parameters
+ * @n: input int
+ *
+ * Return: sum of paramers or 0 if n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	if (n == 0)
+		return (0);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNTIONS_H
+#define VARIADIC_FUNTIONS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <strarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
