f0bc75b90c72da0a9e514b649e0f3163d503eeda
Pronothurah <onsongojosphat28@gmail.com>
2023-09-04 21:43:20 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 #define USAGE "Usage: cp file_from file_to\n"
-#define ERR_NOREAD "Error: Can't read from file NAME_OF_THE_FILE %s\n"
-#define ERR_NOWRITE "Error: Can't write to NAME_OF_THE_FILE %s\n"
-#define ERR_NOCLOSE "Error: Can't close fd FD_VALUE %d\n"
-#define PERMISIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
  * main - entry point
@@ -24,7 +24,7 @@ int main(int ac, char **av)
 	from_file_data = open(av[1], O_RDONLY);
 	if (from_file_data == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	to_file_data = open(av[2], O_RDONLY | O_CREAT | O_TRUNC, PERMISIONS);
+	to_file_data = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_file_data == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
@@ -40,7 +40,7 @@ int main(int ac, char **av)
 	if (from_file_data)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_file_data), exit(100);
 	if (to_file_data)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, to_file_data), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_file_data), exit(100);
 
 	return (EXIT_SUCCESS);
 }
