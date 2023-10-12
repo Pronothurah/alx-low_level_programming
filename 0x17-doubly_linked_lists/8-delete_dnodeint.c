502c64eeab3e9c5412abccfde28d49cdcc7cbefb
Pronothurah <onsongojosphat28@gmail.com>
2023-10-12 13:05:54 +0000

    betty check

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -36,7 +36,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		temp->prev->next = temp->next;
 		if (temp->next != NULL)
 		{
-			temp->next->prev = temp->next;
+			temp->next->prev = temp->prev;
 		}
 	}
 	free(temp);
